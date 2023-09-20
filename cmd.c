eba741abca7c1786a2163580677ba4faaaa04ab4
MOUHCINEHANAFY <hanafymouhcine@gmail.com>
2023-09-20 13:39:23 -0700

    tasks correction

--- cmd.c
+++ cmd.c
@@ -1,43 +1,50 @@
 #include "monty.h"
 
 /**
- * cmd - run the opcode
- * @stack: head linked list - stack
- * @counter: line_counter
- * @file: poiner to monty file
+ * cmd - executes the opcode from the content
  * @content: line content
- * Return: no return
+ * @stack: stack head
+ * @counter: line_number
+ * @file: pointer to monty file
+ * Return: 0 if successful, 1 if error
  */
 int cmd(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
-        instruction_t opst[] = {
-                {"push", push}, {"pall", pall},
-	       	{"pint", pint}, {"pop", pop}, {"swap", swap},
-	       	{"add", add}, {"nop", nop}, {NULL, NULL}
-        };
-        unsigned int i = 0;
-        char *op;
+	unsigned int i = 0;
+	
+    instruction_t opst[] = {
+        {"push", push}, {"pall", pall}, {"pint", pint},
+        {"pop", pop}, {"swap", swap}, {"add", add},
+        {"nop", nop}, {NULL, NULL}
+    };
 
-        op = strtok(content, " \n\t");
-        if (op && op[0] == '#')
-                return (0);
-        montyf.arg = strtok(NULL, " \n\t");
-        while (opst[i].opcode && op)
+    char *op = strtok(content, " \n\t");
+
+    if (!op || op[0] == '#')
+    {
+        return (0);
+    }
+
+    montyf.arg = strtok(NULL, " \n\t");
+
+
+
+    while (opst[i].opcode)
+    {
+        if (strcmp(op, opst[i].opcode) == 0)
         {
-                if (strcmp(op, opst[i].opcode) == 0)
-                {
-                        opst[i].f(stack, counter);
-                        return (0);
-                }
-                i++;
+            opst[i].f(stack, counter);
+            return (0);
         }
-        if (op && opst[i].opcode == NULL)
-        {
-                fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
-                fclose(file);
-                free(content);
-                free_stack(*stack);
-                exit(EXIT_FAILURE);
-        }
-        return (1);
+        i++;
+    }
+
+    fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+    fclose(file);
+    free(content);
+    free_stack(*stack);
+    exit(EXIT_FAILURE);
+
+    return (1);
 }
+
